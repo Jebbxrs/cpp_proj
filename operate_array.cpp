4d6f108a31830af8ff75b6a017f99eaf29d93416
Vince Jevy Tapdasan <143710577+Jebbxrs@users.noreply.github.com>
2023-12-14 00:20:19 +0800

    Update operate_array.cpp

--- operate_array.cpp
+++ operate_array.cpp
@@ -1,41 +1,50 @@
 #include <iostream>
+#include <iomanip>
 
-void input_data(int arr_1[4], float arr_2[4], int arr_3[4]);
-void diff_sum(int arr_1[4], float arr_2[4]);
+void input_data(int arr_1[4], float arr_2[4]);
+void diff_sum(int arr_1[4], float arr_2[4], float arr_3[4]);
+void print_values(int arr_1[4], float arr_2[4], float arr_3[4]);
 
 int main()
 {
-    int arr_1[4], arr_3[4];
-    float arr_2[4];
+    int arr_1[4];
+    float arr_2[4], arr_3[4];
 
-    input_data(arr_1, arr_2, arr_3);
+    input_data(arr_1, arr_2);
+    diff_sum(arr_1, arr_2, arr_3);
+    return 0;
 }
 
-void input_data(int arr_1[4], float arr_2[4], int arr_3[4])
-{
-    for (int i = 0; i < 5; ++i)
-    {
-        std::cout << "Enter integer number " << i + 1 << " : " << std::endl;
-        std::cin >> arr_1[i]; 
-    }
-
-    for (int i = 0; i < 5; ++i)
-    {
-        std::cout << "Enter float number " << i + 1 << " : " << std::endl;
-        std::cin >> arr_2[i]; 
-    }    
-    diff_sum(arr_1, arr_2);
+void input_data(int arr_1[4], float arr_2[4])
+{   
+    std::cout << "Enter 5 integer numbers: ";
+    std::cin >> arr_1[0] >> arr_1[1] >> arr_1[2] >> arr_1[3] >> arr_1[4]; 
+    std::cout << "Enter 5 float numbers: ";
+    std::cin >> arr_2[0] >> arr_2[1] >> arr_2[2] >> arr_2[3] >> arr_2[4];   
+    return;
 }
 
-void diff_sum(int arr_1[4], float arr_2[4])
+void diff_sum(int arr_1[4], float arr_2[4], float arr_3[4])
 {
-    float result[4];
-
-    for (int i = 0, j = 4; i < 4, j >= 0; i++, j--)
+    for (int i = 0, j = 4; i < 5, j >= 0; i++, j--)
     {
         if (arr_1[i] > arr_2[j])
-            result[i] = arr_1[i] - arr_2[j];    
+            arr_3[i] = arr_1[i] - arr_2[j];    
         else 
-            result[i] = arr_1[i] + arr_2[j];
+            arr_3[i] = arr_1[i] + arr_2[j];
     }
+    print_values(arr_1, arr_2, arr_3);
+    return;
+}
+
+void print_values(int arr_1[4], float arr_2[4], float arr_3[4])
+{
+    for (int i = 0, j = 4; i < 5, j >= 0; i++, j--)
+    {
+        if (arr_1[i] > arr_2[j])
+            std::cout << arr_1[i] << " - " << arr_2[j] << " = " << std::fixed << std::setprecision(2) << arr_3[i] << std::endl;
+        else 
+            std::cout << arr_1[i] << " + " << arr_2[j] << " = " << std::fixed << std::setprecision(2) << arr_3[i] << std::endl;
+    }
+    return;
 }
